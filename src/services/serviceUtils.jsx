a1efeb59ad2c0d2577e42ded4212b390d6a62739
Amit Kumar <amitmonu0000@gmail.com>
2024-02-22 18:37:55 +0530

    path update

--- src/services/serviceUtils.jsx
+++ src/services/serviceUtils.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const BaseURL = "http://localhost:8000";
+// export const BaseURL = "http://localhost:8000";
+export const BaseURL = "https://amittodoapp.netlify.app";
 
 export const api = axios.create({
   baseURL: BaseURL,
